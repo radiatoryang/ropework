a6dd7896b5b416b257ebfe35d6db8311def34caa
Robert Yang <yang.robert.w@gmail.com>
2018-08-06 21:36:45 -0400

    implemented <<ACT>> for characters, and doing some dynamic character highlighting based on who's talking too

--- Assets/Ropework/scripts/RopeworkManager.cs
+++ Assets/Ropework/scripts/RopeworkManager.cs
@@ -9,13 +9,20 @@ using Yarn.Unity;
 namespace Ropework {
     public class RopeworkManager : MonoBehaviour
     {
+		[Header("Overrides"), Tooltip("you can manually assign various assets here if you don't want to use /Resources/ folder")]
+		public Sprite[] loadSprites;
+		public AudioClip[] loadAudio;
 
+		[Header("UI")]
 		public Image bgImage;
 		public Image genericSprite;
+		public Color highlightTint, defaultTint;
 		List<Image> sprites = new List<Image>();
-		Dictionary<string,Image> actors = new Dictionary<string,Image>();
+		[HideInInspector] public Dictionary<string,Image> actors = new Dictionary<string,Image>();
 
 		string[] separ = new string[] {","};
+
+		#region YarnCommands
 
 		[YarnCommand("Scene")]
 		public void DoSceneChange(string textureName) {
@@ -24,15 +31,34 @@ namespace Ropework {
 
 		[YarnCommand("Act")]
 		public void SetActor(string parameters) {
+			// get parameter data
 			var par = parameters.Split( separ, System.StringSplitOptions.RemoveEmptyEntries);
-			var textureName = par[1];
-			if ( actors.ContainsKey(par[0])) {
-				actors[par[0]].sprite = Resources.Load<Sprite>(par[1]);
+			var actorName = par[0];
+			var spriteName = par[1];
+
+			// have to use SetSprite() because par[2] and par[3] might be keywords (e.g. "left", "right")
+			var newActor = SetSprite( string.Format("{0},{1},{2}", spriteName, par.Length > 2 ? par[2] : "", par.Length > 3 ? par[3] : "" ) );
+
+			// if the actor is using a sprite already, then maybe clone position, and destroy it
+			if ( actors.ContainsKey(actorName)) {
+				// if any missing position params, assume the actor position should stay the same
+				var newPos = newActor.rectTransform.anchoredPosition;
+				if ( par.Length == 2 ) { // missing 2 params, override both x and y
+					newPos = actors[actorName].rectTransform.anchoredPosition;
+				} else if ( par.Length == 3) { // missing 1 param, override y
+					newPos.y = actors[actorName].rectTransform.anchoredPosition.y;
+				}
+				newActor.rectTransform.anchoredPosition = newPos;
+				// clean-up
+				Destroy( actors[actorName].gameObject );
+				actors.Remove(actorName);
 			}
+
+			actors.Add( actorName, newActor );
 		}
 
 		[YarnCommand("Show")]
-		public void SetSprite(string parameters) {
+		public Image SetSprite(string parameters) {
 			var par = parameters.Split( separ, System.StringSplitOptions.RemoveEmptyEntries);
 
 			// set sprite
@@ -48,30 +74,78 @@ namespace Ropework {
 			}
 
 			// actually instantiate and draw sprite now
-			SetSpriteActual( spriteName, pos );
+			return SetSpriteActual( spriteName, pos );
 		}
 
 		[YarnCommand("Hide")]
 		public void HideSprite(string spriteName) {
-			// let's just do this in a sloppy way for now
-			foreach ( var sprite in sprites ) {
-				if (sprite.name == spriteName) {
-					Destroy(sprite.gameObject);
+			// find the spriteObject with name "spriteName" and destroy it
+			// let's just do this in a sloppy way for now, and also assume there's only one object like it
+			Image toDestroy = null;
+			foreach ( var spriteObject in sprites ) {
+				if (spriteObject.name == spriteName) {
+					toDestroy = spriteObject;
+					break;
 				}
+			}
+			// if an actor is using it, also remove reference to it
+			string keyToRemove = "";
+			foreach ( var kvp in actors ) {
+				if ( kvp.Value == toDestroy ) {
+					keyToRemove = kvp.Key;
+					break;
+				}
+			}
+			// there's probably a better way to do this
+			if ( keyToRemove.Length > 0 ) {
+				actors.Remove(keyToRemove);
+			}
+
+			// don't forget to actually destroy the sprite object
+			Destroy(toDestroy.gameObject);
+			sprites.Remove(toDestroy);
+		}
+
+		#endregion
+
+		#region Utility
+
+		// called by ClassicDialogueUI
+		public void HighlightSprite (Image sprite) {
+			StopCoroutine( "HighlightSpriteCoroutine" ); // use StartCoroutine(string) overload so that we can Stop and Start the coroutine (it doesn't work otherwise?)
+			StartCoroutine( "HighlightSpriteCoroutine", sprite );
+		}
+
+		IEnumerator HighlightSpriteCoroutine (Image highlightedSprite) {
+			float t = 0f;
+			// over time, gradually change sprites to be "normal" or "highlighted"
+			while ( t < 1f ) {
+				t += Time.deltaTime / 2f;
+				foreach ( var spr in sprites ) {
+					if ( spr != highlightedSprite) { // set back to normal
+						spr.transform.localScale = Vector3.MoveTowards( spr.transform.localScale, Vector3.one, Time.deltaTime );
+						spr.color = Color.Lerp( spr.color, defaultTint, Time.deltaTime * 5f );
+					} else { // a little bit bigger / brighter
+						spr.transform.localScale = Vector3.MoveTowards( spr.transform.localScale, Vector3.one * 1.1f, Time.deltaTime );
+						spr.color = Color.Lerp( spr.color, highlightTint, Time.deltaTime * 5f );
+					}
+				}
+				yield return 0;
 			}
 		}
 
-		void SetSpriteActual(string spriteName, Vector2 position) {
+		Image SetSpriteActual(string spriteName, Vector2 position) {
 			var newSpriteObject = Instantiate<Image>(genericSprite, genericSprite.transform.parent);
 			sprites.Add(newSpriteObject);
 			newSpriteObject.name = spriteName;
 			newSpriteObject.sprite = ValidateSprite( spriteName );
 			newSpriteObject.SetNativeSize();
 			newSpriteObject.rectTransform.anchoredPosition = Vector2.Scale( position, new Vector2( 1280f, 720f ) );
+			return newSpriteObject;
 		}
 
 		float ConvertCoordinates(string coordinate) {
-			// first, let's see if they used a keyword
+			// first, let's see if they used a position keyword
 			coordinate = coordinate.ToLower();
 			switch ( coordinate ) {
 				case "left":
@@ -102,6 +176,14 @@ namespace Ropework {
         }
 
 		Sprite ValidateSprite( string spriteName ) {
+			// first, check to see if it's a manually loaded sprite
+			foreach ( var spr in loadSprites ) {
+				if (spr.name == spriteName) {
+					return spr;
+				}
+			}
+
+			// otherwise, let's search Resources for it
 			var newSprite = Resources.Load<Sprite>(spriteName);
 			if ( newSprite == null ) {
 				Debug.LogErrorFormat( "Ropework can't find sprite [{0}]... maybe it is misspelled, or it isn't configured as a sprite?", spriteName );
@@ -110,6 +192,8 @@ namespace Ropework {
 				return newSprite;
 			}
 		}
+
+		#endregion
 
 
     } // end class
